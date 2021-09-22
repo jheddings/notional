6f28db6415549659d5a207ada3d9a71e1d808d38
jheddings <jason@heddings.com>
2021-09-22 08:17:41 -0600

    Initial support to verify Notion connectivity.  Implements #5

---
+++ examples/session.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""This script demonstrates working with the Sessoin object.
+
+The caller must set `NOTION_AUTH_TOKEN` to a valid integration token.
+"""
+
+import logging
+import os
+
+logging.basicConfig(level=logging.INFO)
+
+import notional
+
+auth_token = os.getenv("NOTION_AUTH_TOKEN")
+notion = notional.connect(auth=auth_token)
+
+notion.ping()
