f756225a823681965be4469b54b509acbc5b1148
jheddings <jason@heddings.com>
2021-10-23 11:46:01 -0600

    Removed module dependencies to help with local pip install.  #4

--- setup.py
+++ setup.py
@@ -1,20 +1,33 @@
 import os
 
 from setuptools import setup, find_packages
-from notional.version import __version__
 
-with open("README.md", "r") as fp:
-    readme = fp.read()
+# used from pip's setup.py:
+# https://github.com/pypa/pip/blob/main/setup.py
 
-requirements = ["notion-client>=0.6", "pydantic>=1.8.2"]
+def read_file(rel_path: str) -> str:
+    here = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(here, rel_path)) as fp:
+        return fp.read()
+
+
+def get_version(rel_path: str) -> str:
+    for line in read_file(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
+
+
+requirements = ["notion-client>=0.7", "pydantic>=1.8.2"]
 
 setup(
     name="notional",
-    version=__version__,
+    version=get_version("notional/version.py"),
     author="Jason Heddings",
     author_email="jheddings@gmail.com",
     description="An high-level interface for the Notion SDK.",
-    long_description=readme,
+    long_description=read_file("README.md"),
     long_description_content_type="text/markdown",
     url="https://github.com/jheddings/notional/",
     packages=find_packages(),
