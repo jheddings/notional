7e198eb0fcae4fa94b036f1b8574439229303df7
jheddings <jason@heddings.com>
2022-03-31 07:55:06 -0600

    Converted to html5lib and improved flow handling.
    Fixes #22 and #23.

--- notional/parser.py
+++ notional/parser.py
@@ -1,16 +1,24 @@
 """Utilities for parsing other document types in Notion.
 
-NOTE: this is a _very_ prelimiary implementation and will likely change frequently.
+These parsers provide an alternative to the "Import" cabilities of the Notion client
+and Notion Web Clipper.  Perhaps that capability will be exposed to the API in the
+future, which would effectively render these parsers unnecessary.
 """
+
+# FIXME don't discard implicit text blocks / phrasing elements
+# FIXME add support for links
+# FIXME improve extra spacing around text elements
 
 # TODO add more options for callers to customize output
 # TODO look for options to handle text styled using CSS
-# TODO consider using lxml.etree - reduce dependencies
+# TODO consider how to handle <form> content
 
 import logging
 import re
+from abc import ABC, abstractmethod
+from xml.etree import ElementTree
 
-from bs4 import BeautifulSoup, Comment, Doctype, NavigableString
+import html5lib
 
 from notional.text import Annotations, TextObject
 
@@ -22,240 +30,320 @@ log = logging.getLogger(__name__)
 img_data_re = re.compile("^data:image/([^;]+);([^,]+),(.+)$")
 
 
-def gather_text(elem, strip=True, collapse=True):
-    text = " ".join(elem.strings)
+def normalize_text(text):
+    if text is None:
+        return None
 
-    if strip:
-        text = text.strip()
-
-    if collapse and text:
-        text = re.sub(r"\s+", " ", text, flags=re.MULTILINE)
+    text = re.sub(r"\s+", " ", text, flags=re.MULTILINE)
 
     return text or None
 
 
-class HtmlDocumentParser(object):
-    def __init__(self, base=None):
-        self.base = base
+def gather_text(elem):
+    text = "".join(elem.itertext())
+    text = text.strip()
+    text = normalize_text(text)
+    return text or None
 
+
+class DocumentParser(ABC):
+
+    title: str
+    content: list()
+
+    def __init__(self):
         self.title = None
-        self.meta = dict()
-        self.doctype = None
-        self.comments = list()
         self.content = list()
 
-        # XXX this leads to some limitations with improperly balanced style tags
+    @abstractmethod
+    def parse(self, data):
+        pass
+
+
+class HtmlParser(DocumentParser):
+    """An HTML parser that leverages the WHATWG HTML spec."""
+
+    meta: dict()
+
+    def __init__(self, base=None):
+        super().__init__()
+
+        self._base_url = base
+
+        self.meta = dict()
+
+        self._current_href = None
         self._current_text_style = Annotations()
 
     def parse(self, html):
 
         log.debug("BEGIN parsing")
 
-        soup = BeautifulSoup(html, "html.parser")
-        self._handle_element(soup)
+        # alternative: use lxml.etree interchangeably...
+        # doc = lxml.etree.HTML(html)
+
+        doc = html5lib.parse(html, namespaceHTMLElements=False)
+
+        self._render(doc)
 
         log.debug("END parsing")
 
-    def _handle_element(self, elem, parent=None):
-        log.debug("processing element - %s :: [%s]", elem.name, parent)
+    def _render(self, elem, parent=None):
+        log.debug("rendering element - %s :: [%s]", elem.tag, parent)
 
         if parent is None:
             parent = self.content
 
-        for child in elem.children:
-
-            # keep comments just for fun...
-            if isinstance(child, Comment):
-                text = gather_text(child)
-                self.comments.append(text)
-
-            # grab the doctype just in case..
-            elif isinstance(child, Doctype):
-                self.doctype = gather_text(child)
-
-            # handle text elements...
-            elif isinstance(child, NavigableString):
-                self._handle_text(child, parent)
-
-            # handle known blocks...
-            elif hasattr(self, f"_handle_{child.name}"):
-                log.debug("handler -- _handle_%s", child.name)
-                pfunc = getattr(self, f"_handle_{child.name}")
-                pfunc(child, parent)
-
-            # otherwise, just keep digging...
-            else:
-                self._handle_element(child, parent)
+        if hasattr(self, f"_render_{elem.tag}"):
+            log.debug("handler func -- _render_%s", elem.tag)
+            pfunc = getattr(self, f"_render_{elem.tag}")
+            pfunc(elem, parent)
 
         log.debug("block complete; %d content block(s)", len(self.content))
 
-    def _handle_text(self, elem, parent):
-        if isinstance(parent, blocks.Code):
-            text = gather_text(elem, strip=False, collapse=False)
-        else:
-            text = gather_text(elem, strip=False, collapse=True)
+    def _render_a(self, elem, parent):
+        self._current_href = elem.get("href")
+        self._process_contents(elem, parent=parent)
+        self._current_href = None
 
-        style = self._current_text_style.dict()
-        obj = TextObject.from_value(text, **style)
+    def _render_b(self, elem, parent):
+        self._current_text_style.bold = True
+        self._process_contents(elem, parent=parent)
+        self._current_text_style.bold = False
 
-        if isinstance(parent, blocks.TextBlock):
-            if obj is not None and len(text) > 0:
-                parent.concat(obj)
+    def _render_base(self, elem, parent):
+        base = elem.get("href")
+        if base is not None:
+            self._base_url = base
 
-        elif isinstance(parent, blocks.TableRow):
-            parent.append(obj)
+    def _render_blockquote(self, elem, parent):
+        block = blocks.Quote()
+        self._process_contents(elem, parent=block)
+        parent.append(block)
 
-    def _handle_meta(self, elem, parent):
-        name = elem.get("name")
-        value = elem.get("content")
-        if name and value:
-            self.meta[name] = value
+    def _render_body(self, elem, parent):
+        self._process_contents(elem, parent=parent)
 
-    def _handle_script(self, elem, parent):
-        pass
-
-    def _handle_link(self, elem, parent):
-        pass
-
-    def _handle_style(self, elem, parent):
-        pass
-
-    def _handle_title(self, elem, parent):
-        self.title = gather_text(elem)
-
-    def _handle_br(self, elem, parent):
+    def _render_br(self, elem, parent):
         if isinstance(parent, blocks.TextBlock):
             parent.concat("\n")
 
-    def _handle_hr(self, elem, parent):
-        parent.append(blocks.Divider())
-
-    def _handle_h1(self, elem, parent):
-        heading = blocks.Heading1()
-        self._handle_element(elem, parent=heading)
-        parent.append(heading)
-
-    def _handle_h2(self, elem, parent):
-        heading = blocks.Heading2()
-        self._handle_element(elem, parent=heading)
-        parent.append(heading)
-
-    def _handle_h3(self, elem, parent):
-        heading = blocks.Heading3()
-        self._handle_element(elem, parent=heading)
-        parent.append(heading)
-
-    def _handle_p(self, elem, parent):
-        para = blocks.Paragraph()
-        self._handle_element(elem, parent=para)
-        parent.append(para)
-
-    def _handle_tt(self, elem, parent):
-        self._handle_pre(elem, parent)
-
-    def _handle_pre(self, elem, parent):
-        block = blocks.Code()
-        self._handle_element(elem, parent=block)
-        parent.append(block)
-
-    def _handle_blockquote(self, elem, parent):
-        block = blocks.Quote()
-        self._handle_element(elem, parent=block)
-        parent.append(block)
-
-    def _handle_b(self, elem, parent):
-        self._handle_strong(elem, parent)
-
-    def _handle_strong(self, elem, parent):
-        self._current_text_style.bold = True
-        self._handle_element(elem, parent=parent)
-        self._current_text_style.bold = False
-
-    def _handle_i(self, elem, parent):
-        self._handle_em(elem, parent)
-
-    def _handle_em(self, elem, parent):
-        self._current_text_style.italic = True
-        self._handle_element(elem, parent=parent)
-        self._current_text_style.italic = False
-
-    def _handle_u(self, elem, parent):
-        self._handle_ins(elem, parent)
-
-    def _handle_ins(self, elem, parent):
-        self._current_text_style.underline = True
-        self._handle_element(elem, parent=parent)
-        self._current_text_style.underline = False
-
-    def _handle_strike(self, elem, parent):
-        self._handle_del(elem, parent)
-
-    def _handle_del(self, elem, parent):
-        self._current_text_style.strikethrough = True
-        self._handle_element(elem, parent=parent)
-        self._current_text_style.strikethrough = False
-
-    def _handle_kbd(self, elem, parent):
-        self._handle_code(elem, parent)
-
-    def _handle_code(self, elem, parent):
+    def _render_code(self, elem, parent):
         self._current_text_style.code = True
-        self._handle_element(elem, parent=parent)
+        self._process_contents(elem, parent=parent)
         self._current_text_style.code = False
 
-    def _handle_li(self, elem, parent):
-        if elem.parent.name == "ol":
-            li = blocks.NumberedListItem()
-        else:
-            li = blocks.BulletedListItem()
+    def _render_dd(self, elem, parent):
+        self._process_contents(elem, parent=parent)
 
-        self._handle_element(elem, li)
-        parent.append(li)
+    def _render_del(self, elem, parent):
+        self._render_strike(elem, parent)
 
-    def _handle_dl(self, elem, parent):
+    def _render_div(self, elem, parent):
+        self._process_contents(elem, parent)
+
+    def _render_dl(self, elem, parent):
         dl = blocks.Paragraph()
-        self._handle_element(elem, parent=dl)
+        self._process_contents(elem, parent=dl)
         parent.append(dl)
 
-    def _handle_table(self, elem, parent):
-        table = blocks.Table()
-        self._handle_element(elem, parent=table)
+    def _render_dt(self, elem, parent):
+        self._process_contents(elem, parent=parent)
 
-        if table.Width > 0:
-            parent.append(table)
+    def _render_em(self, elem, parent):
+        self._render_i(elem, parent)
 
-    def _handle_thead(self, elem, parent):
-        parent.table.has_column_header = True
-        self._handle_element(elem, parent)
+    def _render_h1(self, elem, parent):
+        h1 = blocks.Heading1()
+        self._process_contents(elem, parent=h1)
+        parent.append(h1)
 
-    def _handle_tr(self, elem, parent):
-        row = blocks.TableRow()
-        for td in elem("td"):
-            self._handle_element(td, parent=row)
-        parent.append(row)
+    def _render_h2(self, elem, parent):
+        h2 = blocks.Heading2()
+        self._process_contents(elem, parent=h2)
+        parent.append(h2)
 
-    def _handle_object(self, elem, parent):
-        # TODO support 'data' attribute as an embed or upload?
-        self._handle_element(elem, parent)
+    def _render_h3(self, elem, parent):
+        h3 = blocks.Heading3()
+        self._process_contents(elem, parent=h3)
+        parent.append(h3)
 
-    def _handle_iframe(self, elem, parent):
+    def _render_head(self, elem, parent):
+        self._process_contents(elem, parent=parent)
+
+    def _render_hr(self, elem, parent):
+        parent.append(blocks.Divider())
+
+    def _render_html(self, elem, parent):
+        self._process_contents(elem, parent=parent)
+
+    def _render_i(self, elem, parent):
+        self._current_text_style.italic = True
+        self._process_contents(elem, parent=parent)
+        self._current_text_style.italic = False
+
+    def _render_iframe(self, elem, parent):
         src = elem.get("src")
         if src is not None:
             block = blocks.Embed.from_url(src)
             parent.append(block)
 
-    def _handle_img(self, elem, parent):
-        src = elem["src"]
+    def _render_img(self, elem, parent):
+        src = elem.get("src")
 
-        # TODO use self.base for relative paths - or upload as HostedFile?
+        # TODO use self._base_url for relative paths
         # TODO support embedded images (data:image) as HostedFile...
 
-        file = types.ExternalFile.from_url(src)
-        img = blocks.Image(image=file)
+        if src is not None:
+            file = types.ExternalFile.from_url(src)
+            img = blocks.Image(image=file)
 
-        parent.append(img)
+            parent.append(img)
 
-    def _handle_img_data(self, elem):
+    def _render_ins(self, elem, parent):
+        self._render_u(elem, parent)
+
+    def _render_kbd(self, elem, parent):
+        self._render_code(elem, parent)
+
+    def _render_li(self, elem, parent):
+        self._process_contents(elem, parent)
+
+    def _render_menu(self, elem, parent):
+        self._render_ul(elem, parent)
+
+    def _render_meta(self, elem, parent):
+        name = elem.get("name")
+        value = elem.get("content")
+        if name and value:
+            self.meta[name] = value
+
+    def _render_object(self, elem, parent):
+        # XXX support 'data' attribute as an embed or upload?
+        self._process_contents(elem, parent=parent)
+
+    def _render_ol(self, elem, parent):
+        for li in elem.findall("li"):
+            block = blocks.NumberedListItem()
+            self._render(li, parent=block)
+            parent.append(block)
+
+    def _render_p(self, elem, parent):
+        para = blocks.Paragraph()
+        self._process_contents(elem, parent=para)
+        parent.append(para)
+
+    def _render_pre(self, elem, parent):
+        block = blocks.Code()
+        self._process_contents(elem, parent=block)
+        parent.append(block)
+
+    def _render_s(self, elem, parent):
+        self._render_strike(self, elem, parent)
+
+    def _render_samp(self, elem, parent):
+        self._render_code(elem, parent)
+
+    def _render_strike(self, elem, parent):
+        self._current_text_style.strikethrough = True
+        self._process_contents(elem, parent=parent)
+        self._current_text_style.strikethrough = False
+
+    def _render_strong(self, elem, parent):
+        self._render_b(elem, parent)
+
+    def _render_table(self, elem, parent):
+        table = blocks.Table()
+        self._process_contents(elem, parent=table)
+
+        if table.Width > 0:
+            parent.append(table)
+
+    def _render_tbody(self, elem, parent):
+        self._process_contents(elem, parent)
+
+    def _render_td(self, elem, parent):
+        if elem.text is None:
+            self._process_text(" ", parent)
+        else:
+            self._process_contents(elem, parent=parent)
+
+    def _render_tfoot(self, elem, parent):
+        self._process_contents(elem, parent)
+
+    def _render_th(self, elem, parent):
+        self._render_td(elem, parent=parent)
+
+    def _render_thead(self, elem, parent):
+        parent.table.has_column_header = True
+        self._process_contents(elem, parent=parent)
+
+    def _render_title(self, elem, parent):
+        self.title = gather_text(elem)
+
+    def _render_tr(self, elem, parent):
+        row = blocks.TableRow()
+        for td in elem.findall("td"):
+            self._render(td, parent=row)
+        parent.append(row)
+
+    def _render_tt(self, elem, parent):
+        self._render_pre(elem, parent)
+
+    def _render_u(self, elem, parent):
+        self._current_text_style.underline = True
+        self._process_contents(elem, parent=parent)
+        self._current_text_style.underline = False
+
+    def _render_ul(self, elem, parent):
+        for li in elem.findall("li"):
+            block = blocks.BulletedListItem()
+            self._render(li, parent=block)
+            parent.append(block)
+
+    def _render_var(self, elem, parent):
+        self._render_code(elem, parent)
+
+    def _find_and_render(self, parent, elem, *tags):
+        for tag in tags:
+            child = elem.find(tag)
+            if tag is not None:
+                self._render(child, parent)
+
+    def _process_text(self, text, parent):
+        if not isinstance(parent, blocks.Code):
+            text = normalize_text(text)
+
+        style = self._current_text_style.dict()
+        obj = TextObject.from_value(text, **style)
+
+        if isinstance(parent, blocks.TextBlock):
+            if obj is not None:
+                parent.concat(obj)
+
+        elif isinstance(parent, blocks.TableRow):
+            parent.append(obj)
+
+    def _process_contents(self, elem, parent):
+        log.debug("processing contents :: %s [%s]", elem, parent)
+
+        #if isinstance(parent, blocks.TextBlock):
+        #    text_parent = parent
+        #    implicit_text = False
+        #else:
+        #    text_parent = blocks.Paragraph()
+        #    implicit_text = True
+
+        self._process_text(elem.text, parent)
+
+        for child in elem:
+            self._render(child, parent)
+            self._process_text(child.tail, parent)
+
+        #if implicit_text:
+        #    parent.append(text_parent)
+
+    def _process_img_data(self, elem):
         import base64
         import tempfile
 
