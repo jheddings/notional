6f8b1f31dbfd19332952484ebd161641e4205f13
jheddings <jason@heddings.com>
2022-04-25 20:19:40 -0600

    Version bump.

--- notional/version.py
+++ notional/version.py
@@ -5,7 +5,7 @@ import os
 
 log = logging.getLogger(__name__)
 
-__version__ = "0.3.2"
+__version__ = "0.4.0"
 
 # if we are running in a local copy, append the repo information
 # XXX do we want to do something more advanced when using `make publish` ?
