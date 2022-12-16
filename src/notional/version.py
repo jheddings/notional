73aeb6816c5208490b7935930ec2c7906a33cb20
jheddings <jason@heddings.com>
2022-12-16 07:39:55 -0700

    Simplified version logging.

--- src/notional/version.py
+++ src/notional/version.py
@@ -9,15 +9,12 @@ logger = logging.getLogger(__name__)
 
 __pkgname__ = "notional"
 
-logger.debug("loading package distribution")
 _pkg_dist = pkg_resources.get_distribution(__pkgname__)
 
 __version__ = _pkg_dist.version
 
+# if we are running in a local copy, append the repo information
 try:
-
-    # if we are running in a local copy, append the repo information
-    logger.debug("attempting to parse git repo information")
 
     import git
 
@@ -26,20 +23,15 @@ try:
     _srcdir = os.path.dirname(os.path.abspath(__file__))
     _basedir = os.path.abspath(os.path.join(_srcdir, "..", ".."))
 
-    logger.debug("version basedir: %s", _basedir)
-
     try:
         _repo = git.Repo(_basedir)
         _head = _repo.head.commit
 
         assert not _repo.bare
 
-        logger.debug("using repo dir: %s", _repo.git_dir)
-
         __version__ += "-" + _head.hexsha[:7]
 
         _branch = _repo.active_branch.name
-        logger.debug("current git branch: %s", _branch)
 
         if _branch != "main":
             __version__ += "-" + _branch
@@ -50,8 +42,10 @@ try:
     except git.InvalidGitRepositoryError:
         pass
 
+# if python-git is not installed...
 except ModuleNotFoundError:
-    logger.debug("Could not find module")
+    logger.debug("repository information not available")
+    pass
 
 except Exception:
     logger.exception("Unexpected exception while looking for version information.")
