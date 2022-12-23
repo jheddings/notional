25c22753ca3f3cab193d999c3063580bda397476
jheddings <jason@heddings.com>
2022-12-23 10:01:59 -0700

    Improved references.

--- src/notional/session.py
+++ src/notional/session.py
@@ -14,7 +14,7 @@ from .orm import ConnectedPage
 from .query import QueryBuilder, ResultSet
 from .schema import PropertyObject
 from .text import TextObject
-from .types import ObjectReference, ParentRef, Title
+from .types import DatabaseRef, ObjectReference, PageRef, ParentRef, Title
 from .user import User
 
 logger = logging.getLogger(__name__)
@@ -28,10 +28,79 @@ class SessionError(Exception):
         super().__init__(message)
 
 
+class Session(object):
+    """An active session with the Notion SDK."""
+
+    def __init__(self, **kwargs):
+        """Initialize the `Session` object and the endpoints.
+
+        `kwargs` will be passed direction to the Notion SDK Client.  For more details,
+        see the (full docs)[https://ramnes.github.io/notion-sdk-py/reference/client/].
+
+        :param auth: bearer token for authentication
+        """
+        self.client = notion_client.Client(**kwargs)
+
+        self.blocks = BlocksEndpoint(self)
+        self.databases = DatabasesEndpoint(self)
+        self.pages = PagesEndpoint(self)
+        self.search = SearchEndpoint(self)
+        self.users = UsersEndpoint(self)
+
+        logger.info("Initialized Notion SDK client")
+
+    @property
+    def IsActive(self):
+        """Determine if the current session is active.
+
+        The session is considered "active" if it has not been closed.  This does not
+        determine if the session can connect to the Notion API.
+        """
+        return self.client is not None
+
+    def close(self):
+        """Close the session and release resources."""
+
+        if self.client is None:
+            raise SessionError("Session is not active.")
+
+        self.client.close()
+        self.client = None
+
+    def ping(self) -> bool:
+        """Confirm that the session is active and able to connect to Notion.
+
+        Raises SessionError if there is a problem, otherwise returns True.
+        """
+
+        if self.IsActive is False:
+            return False
+
+        error = None
+
+        try:
+
+            me = self.users.me()
+
+            if me is None:
+                raise SessionError("Unable to get current user")
+
+        except ConnectError:
+            error = "Unable to connect to Notion"
+
+        except APIResponseError as err:
+            error = str(err)
+
+        if error is not None:
+            raise SessionError(error)
+
+        return True
+
+
 class Endpoint(object):
     """Notional wrapper for the API endpoints."""
 
-    def __init__(self, session):
+    def __init__(self, session: Session):
         """Initialize the `Endpoint` for the supplied session."""
         self.session = session
 
@@ -47,7 +116,7 @@ class BlocksEndpoint(Endpoint):
             return self.session.client.blocks.children
 
         # https://developers.notion.com/reference/patch-block-children
-        def append(self, parent, *blocks):
+        def append(self, parent, *blocks: Block):
             """Add the given blocks as children of the specified parent.
 
             The blocks info will be refreshed based on returned data.
@@ -201,24 +270,34 @@ class DatabasesEndpoint(Endpoint):
         return request
 
     # https://developers.notion.com/reference/create-a-database
-    def create(self, parent: ParentRef, schema: Dict[str, PropertyObject], title=None):
-        """Add a database to the given Page parent."""
+    def create(self, parent, schema: Dict[str, PropertyObject], title=None):
+        """Add a database to the given Page parent.
 
-        logger.info("Creating database %s - %s", parent, title)
+        `parent` may be any suitable `PageRef` type.
+        """
 
-        request = self._build_request(parent, schema, title)
+        parent_ref = PageRef[parent]
+
+        logger.info("Creating database @ %s - %s", parent_ref.page_id, title)
+
+        request = self._build_request(parent_ref, schema, title)
 
         data = self().create(**request)
 
         return Database.parse_obj(data)
 
     # https://developers.notion.com/reference/retrieve-a-database
-    def retrieve(self, database_id):
-        """Return the Database with the given ID."""
+    def retrieve(self, dbref):
+        """Return the Database with the given ID.
 
-        logger.info("Retrieving database :: %s", database_id)
+        `dbref` may be any suitable `DatabaseRef` type.
+        """
 
-        data = self().retrieve(database_id)
+        dbid = DatabaseRef[dbref].database_id
+
+        logger.info("Retrieving database :: %s", dbid)
+
+        data = self().retrieve(dbid)
 
         return Database.parse_obj(data)
 
@@ -228,10 +307,10 @@ class DatabasesEndpoint(Endpoint):
 
         The database info will be refreshed to the latest version from the server.
 
-        `dbref` may be any suitable `ObjectReference` type.
+        `dbref` may be any suitable `DatabaseRef` type.
         """
 
-        dbid = ObjectReference[dbref].id
+        dbid = DatabaseRef[dbref].database_id
 
         logger.info("Updating database info :: %s", dbid)
 
@@ -243,27 +322,41 @@ class DatabasesEndpoint(Endpoint):
 
         return dbref
 
-    def delete(self, database):
-        """Delete (archive) the specified Database."""
-        logger.info("Deleting database :: %s", database.id)
-        return self.session.blocks.delete(database)
+    def delete(self, dbref):
+        """Delete (archive) the specified Database.
 
-    def restore(self, database):
-        """Restore (unarchive) the specified Database."""
-        logger.info("Restoring database :: %s", database.id)
-        return self.session.blocks.restore(database)
+        `dbref` may be any suitable `DatabaseRef` type.
+        """
+
+        dbid = DatabaseRef[dbref].database_id
+
+        logger.info("Deleting database :: %s", dbid)
+
+        return self.session.blocks.delete(dbid)
+
+    def restore(self, dbref):
+        """Restore (unarchive) the specified Database.
+
+        `dbref` may be any suitable `DatabaseRef` type.
+        """
+
+        dbid = DatabaseRef[dbref].database_id
+
+        logger.info("Restoring database :: %s", dbid)
+
+        return self.session.blocks.restore(dbid)
 
     # https://developers.notion.com/reference/post-database-query
     def query(self, target):
         """Initialize a new Query object with the target data class.
 
-        :param target: either an `ObjectReference` or an ORM class
+        :param target: either a `DatabaseRef` type or an ORM class
         """
 
         if issubclass(target, ConnectedPage):
             dbid = target._notional__database
         else:
-            dbid = ObjectReference[target].id
+            dbid = DatabaseRef[target].database_id
 
         logger.info("Initializing database query :: {%s}", dbid)
 
@@ -318,16 +411,29 @@ class PagesEndpoint(Endpoint):
         return Page.parse_obj(data)
 
     def delete(self, page):
-        """Delete (archive) the specified Page."""
+        """Delete (archive) the specified Page.
+
+        `page` may be any suitable `PageRef` type.
+        """
+
         return self.set(page, archived=True)
 
     def restore(self, page):
-        """Restore (unarchive) the specified Page."""
+        """Restore (unarchive) the specified Page.
+
+        `page` may be any suitable `PageRef` type.
+        """
+
         return self.set(page, archived=False)
 
     # https://developers.notion.com/reference/retrieve-a-page
-    def retrieve(self, page_id):
-        """Return the Page with the given ID."""
+    def retrieve(self, page):
+        """Return the requested Page.
+
+        `page` may be any suitable `PageRef` type.
+        """
+
+        page_id = PageRef[page].page_id
 
         logger.info("Retrieving page :: %s", page_id)
 
@@ -336,7 +442,7 @@ class PagesEndpoint(Endpoint):
         return Page.parse_obj(data)
 
     # https://developers.notion.com/reference/patch-page
-    def update(self, page, **properties):
+    def update(self, page: Page, **properties):
         """Update the Page object properties on the server.
 
         If `properties` are provided, only those values will be updated.  If
@@ -364,29 +470,37 @@ class PagesEndpoint(Endpoint):
     def set(self, page, cover=False, icon=False, archived=None):
         """Set specific page attributes (such as cover, icon, etc) on the server.
 
+        `page` may be any suitable `PageRef` type.
+
         To remove an attribute, set its value to None.
         """
 
+        page_id = PageRef[page].page_id
+
+        props = {}
+
         if cover is None:
-            logger.info("Removing page cover :: %s", page.id)
-            data = self().update(page.id.hex, cover={})
+            logger.info("Removing page cover :: %s", page_id)
+            props["cover"] = {}
         elif cover is not False:
-            logger.info("Setting page cover :: %s => %s", page.id, cover)
-            data = self().update(page.id.hex, cover=cover.to_api())
+            logger.info("Setting page cover :: %s => %s", page_id, cover)
+            props["cover"] = cover.to_api()
 
         if icon is None:
-            logger.info("Removing page icon :: %s", page.id)
-            data = self().update(page.id.hex, icon={})
+            logger.info("Removing page icon :: %s", page_id)
+            props["icon"] = {}
         elif icon is not False:
-            logger.info("Setting page icon :: %s => %s", page.id, icon)
-            data = self().update(page.id.hex, icon=icon.to_api())
+            logger.info("Setting page icon :: %s => %s", page_id, icon)
+            props["icon"] = icon.to_api()
 
         if archived is False:
-            logger.info("Restoring page :: %s", page.id)
-            data = self().update(page.id.hex, archived=False)
+            logger.info("Restoring page :: %s", page_id)
+            props["archived"] = False
         elif archived is True:
-            logger.info("Archiving page :: %s", page.id)
-            data = self().update(page.id.hex, archived=True)
+            logger.info("Archiving page :: %s", page_id)
+            props["archived"] = True
+
+        data = self().update(page_id.hex, **props)
 
         return page.refresh(**data)
 
@@ -447,72 +561,3 @@ class UsersEndpoint(Endpoint):
         data = self().me()
 
         return User.parse_obj(data)
-
-
-class Session(object):
-    """An active session with the Notion SDK."""
-
-    def __init__(self, **kwargs):
-        """Initialize the `Session` object and the endpoints.
-
-        `kwargs` will be passed direction to the Notion SDK Client.  For more details,
-        see the (full docs)[https://ramnes.github.io/notion-sdk-py/reference/client/].
-
-        :param auth: bearer token for authentication
-        """
-        self.client = notion_client.Client(**kwargs)
-
-        self.blocks = BlocksEndpoint(self)
-        self.databases = DatabasesEndpoint(self)
-        self.pages = PagesEndpoint(self)
-        self.search = SearchEndpoint(self)
-        self.users = UsersEndpoint(self)
-
-        logger.info("Initialized Notion SDK client")
-
-    @property
-    def IsActive(self):
-        """Determine if the current session is active.
-
-        The session is considered "active" if it has not been closed.  This does not
-        determine if the session can connect to the Notion API.
-        """
-        return self.client is not None
-
-    def close(self):
-        """Close the session and release resources."""
-
-        if self.client is None:
-            raise SessionError("Session is not active.")
-
-        self.client.close()
-        self.client = None
-
-    def ping(self):
-        """Confirm that the session is active and able to connect to Notion.
-
-        Raises SessionError if there is a problem, otherwise returns True.
-        """
-
-        if self.IsActive is False:
-            return False
-
-        error = None
-
-        try:
-
-            me = self.users.me()
-
-            if me is None:
-                raise SessionError("Unable to get current user")
-
-        except ConnectError:
-            error = "Unable to connect to Notion"
-
-        except APIResponseError as err:
-            error = str(err)
-
-        if error is not None:
-            raise SessionError(error)
-
-        return True
