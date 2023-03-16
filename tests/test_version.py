17a5c7b9717d0bb16027a5aead14dd73fdeb3e97
jheddings <jason@heddings.com>
2023-03-16 17:54:33 -0600

    Better version handling.

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,6 +1,7 @@
 """Unit tests for Notional version information."""
 
 import logging
+import re
 import subprocess
 import sys
 from unittest import mock
@@ -13,8 +14,13 @@ logging.basicConfig(level=logging.INFO)
 
 def test_basic_version():
     """Ensure basic version information is available."""
-    assert version.__pkgname__ is not None
-    assert version.__version__ is not None
+    assert version.__pkgname__
+    assert version.__version__
+
+
+def test_version_is_valid_string():
+    """Ensure version strings meet expected conventions."""
+    assert re.match(r"^[0-9]+(\.[0-9])+(-.*)?$", version.__version__)
 
 
 def test_module_version():
